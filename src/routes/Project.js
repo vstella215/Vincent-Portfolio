7b0203ab455935cc72f67cc3ec920802bc564062
Vincent <vincent@Vincents-MacBook-Air.local>
2024-07-31 00:13:11 -0400

    Price Cards

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Heroimg2 from "../components/Heroimg2";
+import PricingCard from "../components/PricingCard";
 
 
 const Project = () => {
@@ -9,6 +10,7 @@ const Project = () => {
         <div>
             <Navbar />
             <Heroimg2 heading="PROJECTS." text="Some of my most recent works"/>
+            <PricingCard />
             <Footer />
         </div>
     )
