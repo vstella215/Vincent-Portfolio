c7c7b6c6e3444e6dc0a62dbd5273130ed9e38ac0
Vincent <vincent@Vincents-MacBook-Air.local>
2024-08-10 23:45:56 -0400

    Contact Form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Heroimg2 from "../components/Heroimg2";
+import Form from "../components/Form";
 
 
 const Contact = () => {
@@ -9,6 +10,7 @@ const Contact = () => {
         <div>
              <Navbar />
              <Heroimg2 heading="HIRE ME." text="Lets Connect!"/>
+             <Form />
              <Footer />
         </div>
     )
